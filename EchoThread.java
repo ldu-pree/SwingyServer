e24d116c0eace6a5d00ae1bb01312ade906b9f96
ldu-pree <48067817+ldu-pree@users.noreply.github.com>
2020-09-25 22:05:31 +0200

    update

---
+++ EchoThread.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class EchoThread extends Thread {
+    protected Socket socket;
+
+    public EchoThread(Socket clientSocket) {
+        this.socket = clientSocket;
+    }
+
+	@Override
+    public void run() {
+        InputStream inp = null;
+        BufferedReader brinp = null;
+        DataOutputStream out = null;
+        try {
+            inp = socket.getInputStream();
+            brinp = new BufferedReader(new InputStreamReader(inp));
+            out = new DataOutputStream(socket.getOutputStream());
+        } catch (IOException e) {
+            return;
+        }
+        String line = "";
+        while (true) {
+            try {
+                line = brinp.readLine();
+                if ((line == null) || line.equalsIgnoreCase("QUIT")) {
+					System.out.println("Client Says Bye");
+                    socket.close();
+                    return;
+                } else {
+					System.out.println("Client Message: "+line);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                return;
+            }
+        }
+    }
+}
--- Server.java
+++ Server.java
@@ -1,23 +1,23 @@
 import java.net.*;  
 import java.io.*;  
-class Server{  
-	public static void main(String args[])throws Exception{  
-		ServerSocket ss = new ServerSocket(3333);  
-		Socket s = ss.accept();  
-		DataInputStream din=new DataInputStream(s.getInputStream());  
-		DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));  
-		
-		String str="",str2="";  
-		while(!str.equals("stop")){  
-			str=din.readUTF();  
-			System.out.println("client says: "+str);  
-			str2=br.readLine();  
-			dout.writeUTF(str2);  
-			dout.flush();  
-		}  
-		din.close();  
-		s.close();  
-		ss.close();  
+class Server{
+	static final int PORT = 3333;
+	public static void main(String args[])throws Exception{ 
+		ServerSocket serverSocket = null;
+		Socket socket = null;
+		try {
+            serverSocket = new ServerSocket(PORT);
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
+		while (true) {
+            try {
+                socket = serverSocket.accept();
+            } catch (IOException e) {
+                System.out.println("I/O error: " + e);
+            }
+			new EchoThread(socket).start();
+        }
 	}
-}  
+}
